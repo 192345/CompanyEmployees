905d5d520c77c6855fb20152959d5357804a9f58
Gus <floresgalindog@gmail.com>
2026-02-15 17:35:01 -0800

    Services, contracts, entities, dtos, dbcontext, repositories y controllers

--- CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -1,5 +1,21 @@
-﻿namespace CompanyEmployees.ContextFactory;
+﻿//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
 
-public class RepositoryContextFactory
+namespace CompanyEmployees.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
 {
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+            b => b.MigrationsAssembly("CompanyEmployees"));
+        return new RepositoryContext(builder.Options);
+    }
 }
--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -10,8 +10,12 @@ LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentD
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
-
-builder.Services.AddControllers();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
 
